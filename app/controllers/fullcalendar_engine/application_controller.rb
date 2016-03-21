6e0f686f14ba9d07b140de8f443cdbd0c1b1df03
Dan Moore <bitbucket@mooreds.com>
2016-03-21 18:20:46 +0000

    one more needed method

--- app/controllers/fullcalendar_engine/application_controller.rb
+++ app/controllers/fullcalendar_engine/application_controller.rb
@@ -1,6 +1,9 @@
 module FullcalendarEngine
   class ApplicationController < ::ApplicationController 
     skip_before_filter :verify_authenticity_token
+    def set_homepage_path
+      "/"
+    end
 
   end
 end
