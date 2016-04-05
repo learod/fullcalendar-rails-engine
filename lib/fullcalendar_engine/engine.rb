8c4ecb53d28693543c498003bd75241c69dd3ecf
Dan Moore <bitbucket@mooreds.com>
2016-04-05 21:35:56 +0000

    allow decorators so we can modify the models and controllers by using class_eval

--- lib/fullcalendar_engine/engine.rb
+++ lib/fullcalendar_engine/engine.rb
@@ -1,5 +1,12 @@
 module FullcalendarEngine
   class Engine < ::Rails::Engine
     isolate_namespace FullcalendarEngine
+
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
+
   end
 end
