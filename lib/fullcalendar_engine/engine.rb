5750209e9844113033b90da464dfa6655e546a2b
Dan Moore <bitbucket@mooreds.com>
2016-04-07 19:49:40 +0000

    try to get assets to be added to the asset pipeline

--- lib/fullcalendar_engine/engine.rb
+++ lib/fullcalendar_engine/engine.rb
@@ -3,9 +3,15 @@ module FullcalendarEngine
     isolate_namespace FullcalendarEngine
 
     config.to_prepare do
+      # prepare for app to decorate our models and controllers
       Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
         require_dependency(c)
       end
+      # make sure our assets are added to the asset pipeline, per https://gist.github.com/parndt/11381872
+
+      Rails.application.config.assets.precompile += %w(
+        fullcalendar_engine/*
+      )
     end
 
   end
