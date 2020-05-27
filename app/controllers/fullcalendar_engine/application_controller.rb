d8ddb89a4cb7ddb846aa5ddbb83b604561d1cc6a
Carlos Barbiero <carlosbarbiero@gmail.com>
2020-05-27 18:29:38 -0400

    fixed deprecations for rails 5

--- app/controllers/fullcalendar_engine/application_controller.rb
+++ app/controllers/fullcalendar_engine/application_controller.rb
@@ -1,6 +1,6 @@
 module FullcalendarEngine
   class ApplicationController < ::ApplicationController 
-    skip_before_filter :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
     def set_homepage_path
       "/"
     end
