3e75a0beee91d9be2f668d93bb50091bf4ae4e92
Carlos Barbiero <carlosbarbiero@gmail.com>
2020-06-02 19:14:40 -0400

    updated models

--- app/models/fullcalendar_engine/event.rb
+++ app/models/fullcalendar_engine/event.rb
@@ -15,22 +15,22 @@ module FullcalendarEngine
       :months    => "Monthly",
       :years     => "Yearly"
     }
-    
+
     def validate_timings
       if starttime.present? and endtime.present?
         if (starttime >= endtime) and !all_day
-          errors[:base] << "Start Time must be less than End Time"
+          errors.add(:base, message: "Start Time must be less than End Time")
         end
       end
     end
 
     def update_events(events, event)
       events.each do |e|
-        begin 
+        begin
           old_start_time, old_end_time = e.starttime, e.endtime
           e.attributes = event
-          if event_series.period.downcase == 'monthly' or event_series.period.downcase == 'yearly'
-            new_start_time = make_date_time(e.starttime, old_start_time) 
+          if event_series.period.downcase == 'monthly' || event_series.period.downcase == 'yearly'
+            new_start_time = make_date_time(e.starttime, old_start_time)
             new_end_time   = make_date_time(e.endtime, old_end_time, e.endtime)
           else
             new_start_time = make_date_time(e.starttime, old_end_time)
@@ -44,15 +44,15 @@ module FullcalendarEngine
           e.save
         end
       end
-      
+
       event_series.attributes = event
       event_series.save
     end
 
     private
 
-      def make_date_time(original_time, difference_time, event_time = nil)
-        DateTime.parse("#{original_time.hour}:#{original_time.min}:#{original_time.sec}, #{event_time.try(:day) || difference_time.day}-#{difference_time.month}-#{difference_time.year}")
-      end 
+    def make_date_time(original_time, difference_time, event_time = nil)
+      DateTime.parse("#{original_time.hour}:#{original_time.min}:#{original_time.sec}, #{event_time.try(:day) || difference_time.day}-#{difference_time.month}-#{difference_time.year}")
+    end
   end
 end
